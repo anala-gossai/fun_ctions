1cf6de5ab720f941b9c7360829b1aab6eba8e565
analagossai <anala.gossai@gmail.com>
2017-12-02 12:58:02 -0500

    Adjusted spacing

--- PasteNA.R
+++ PasteNA.R
@@ -17,9 +17,9 @@ PasteNA <- function(...,
     L <- list(...)
     
     L <- lapply(L, function(x) {x[is.na(x)] <- ""; x})
-    ret <-gsub(paste0("(^", sep, "|", sep, "$)"), "",
-               gsub(paste0(sep, sep), sep,
-                    do.call(paste, c(L, list(sep = sep)))))
+    ret <- gsub(paste0("(^", sep, "|", sep, "$)"), "",
+                gsub(paste0(sep, sep), sep,
+                     do.call(paste, c(L, list(sep = sep)))))
     is.na(ret) <- ret == ""
     
     ret
